ce6bee4420b71c9fa2c24858bc76646d1aff568f
BANER <86590991+BANSAFAn@users.noreply.github.com>
2024-05-01 12:52:09 +0300

    The “HELP” command
    So far 2 languages, but you can translate instead of me into Russian or something else possible, and then I'll link to the bot !

---
+++ help/help-gl.rs
@@ -0,0 +1,58 @@
+{
+    "embeds": [
+     {
+      "author": {
+       "name": "Good afternoon !"
+      },
+      "title": "Here are some frequently asked questions",
+      "fields": [
+       {
+        "name": "I want to contact the developer !",
+        "value": "If you've read the rulebook, we answered that question there, but we'll say he can answer it in chat ",
+        "inline": false
+       },
+       {
+        "name": "Need help on an error/problem !",
+        "value": "All your claims, bugs and problems can write here in chat ",
+        "inline": false
+       },
+       {
+        "name": "What to do if the owner didn't let you eat ?",
+        "value": "Then you don't deserve it !!!",
+        "inline": false
+       },
+       {
+        "name": "I want to help with translation / correct errors in translation !",
+        "value": "Our server is open source and anyone (who is registered in github and subscribed to our page), can help us with this !",
+        "inline": false
+       }
+      ],
+      "color": "#ff0000"
+     }
+    ],
+    "components": [
+     {
+      "type": 1,
+      "components": [
+       {
+        "type": 2,
+        "style": 5,
+        "label": "Github",
+        "emoji": {
+         "name": "🌴"
+        },
+        "url": "https://github.com/LiveOne-Discord-server"
+       },
+       {
+        "type": 2,
+        "style": 5,
+        "label": "Update Local ",
+        "emoji": {
+         "name": "🎋"
+        },
+        "url": "https://github.com/LiveOne-Discord-server/LiveOne-Bot"
+       }
+      ]
+     }
+    ]
+   }
